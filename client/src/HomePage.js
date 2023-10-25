bb89b3cfad7f9bde8a260f1bb0f71572033ef46b
colinscott0 <116040979+colinscott0@users.noreply.github.com>
2023-10-24 22:57:14 -0400

    Create header and add a few icons

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>
-      <h2>Welcome</h2>
-    </div>
+    <header className="page-header">
+      <img src="/logo.png" alt="logo" className="logo" />
+      <div className="header-icons">
+        <button className="home-button">
+          <img src="/homeButton.png" alt="home" />
+        </button>
+        <button className="message-button">
+          <img src="/messageButton.png"  alt="messages" />
+        </button>
+        <button className="notification-button">
+          <img src="/notiBell.png"  alt="notifications" />
+        </button>
+      </div>
+    </header>
   );
 }
 
