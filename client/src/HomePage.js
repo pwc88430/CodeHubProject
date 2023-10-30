06c469d1877b3459db38f0e8b0c971038e93d00d
colinscott0 <116040979+colinscott0@users.noreply.github.com>
2023-10-30 18:45:48 -0400

    Basic Header added

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -4,16 +4,35 @@ import './HomePage.css';
 function HomePage() {
   return (
     <header className="page-header">
-      <img src="/logo.png" alt="logo" className="logo" />
-      <div className="header-icons">
+      <div className="left-content">
+        <img src="/logo.png" alt="logo" className="logo" />
+        <div className="title">
+        <h1>VOX</h1>
+        <span>SOCIALIS</span>
+        </div>
         <button className="home-button">
           <img src="/homeButton.png" alt="home" />
         </button>
+        <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <button className="search-button">
+          <img src="/searchIcon.png" alt="search" />
+        </button>
+      </div>
+      </div>
+      <div className="right-content">
         <button className="message-button">
-          <img src="/messageButton.png"  alt="messages" />
+          <img src="/messageButton.png" alt="messages" />
         </button>
         <button className="notification-button">
-          <img src="/notiBell.png"  alt="notifications" />
+          <img src="/notiBell.png" alt="notifications" />
+        </button>
+        <button className="profile-button">
+          <img src="/user.png" alt="userPFP" />
+        </button>
+        <p>Username</p>
+        <button className="arrow">
+          <img src="/notificationArrow.png" alt="userPFP" />
         </button>
       </div>
     </header>
@@ -21,3 +40,4 @@ function HomePage() {
 }
 
 export default HomePage;
+
