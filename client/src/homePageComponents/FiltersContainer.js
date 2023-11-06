86828b35851dce2c588cfdb6134d18ac23d74148
Your Name <wilber1285@gmail.com>
2023-11-05 21:59:36 -0500

    minor changes to my recordings container

---
+++ client/src/homePageComponents/FiltersContainer.js
@@ -0,0 +1,11 @@
+import "./FiltersContainer.css";
+
+export default function FiltersContainer() {
+    return (
+        <div id="filters-container">
+            <div className="filter">Recent</div>
+            <div className="filter">Popular</div>
+            <div className="filter">Recomended</div>
+        </div>
+    );
+}
