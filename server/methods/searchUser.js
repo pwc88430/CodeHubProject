adc2b73907d487b71c539828b3dc3076a74eddb6
Colton <81594541+coltonk1@users.noreply.github.com>
2023-11-15 14:24:16 -0500

    search small

--- server/methods/searchUser.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var Helper = require("./other/Helper");
-
-router.post("/", async (req, res) => {
-    // TODO: implement
-});
-
-module.exports = router;
